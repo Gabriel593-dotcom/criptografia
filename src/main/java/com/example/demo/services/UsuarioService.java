1f3859b3913fb7894de377a261e5164638514373
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2022-10-14 11:47:57 -0300

    implementando usuario UserDetails e UserDetailsService

--- src/main/java/com/example/demo/services/UsuarioService.java
+++ src/main/java/com/example/demo/services/UsuarioService.java
@@ -4,14 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.data.DetalheUsuarioData;
 import com.example.demo.model.Usuario;
 import com.example.demo.repositories.UsuarioRepository;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService {
 
 	@Autowired
 	private UsuarioRepository repo;
@@ -26,23 +30,29 @@ public class UsuarioService {
 		return repo.findAll();
 	}
 
-	public Optional<Usuario> findByLogin(String login) {
-		return repo.findByLogin(login);
-	}
-
 	public Usuario save(Usuario obj) {
 		obj.setId(null);
 		obj.setPassword(encoder.encode(obj.getPassword()));
 		repo.save(obj);
 		return obj;
 	}
-	
+
 	public boolean passwordValidation(String login, String password) {
 		Optional<Usuario> obj = repo.findByLogin(login);
-		if(obj.isEmpty()) {
+		if (obj.isEmpty()) {
 			throw new IllegalArgumentException("Usuario inválido");
 		}
 		boolean isMatched = encoder.matches(password, obj.get().getPassword());
-		return isMatched; 
+		return isMatched;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Usuario> obj = repo.findByLogin(username);
+		if (obj.isEmpty()) {
+			throw new UsernameNotFoundException("Usuário [" + username + " ] não encontrado.");
+		}
+
+		return new DetalheUsuarioData(obj);
 	}
 }
